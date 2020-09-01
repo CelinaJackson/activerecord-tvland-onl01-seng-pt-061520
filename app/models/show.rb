1a0658ca5314d704077f75228f9430218e29c4bf
Celina Jackson <celinanjackson@gmail.com>
2020-09-01 00:21:23 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,6 +3,6 @@ class Show < ActiveRecord::Base
   has_many :characters
   
   def actors_list
-    "#{self.first_name} #{self.last_name}"
+    "#{self.all.first_name} #{self.all.last_name}"
   end 
 end
